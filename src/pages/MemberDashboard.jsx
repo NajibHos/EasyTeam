c399814b81e6b575e23b087c64e67d83a8e2e314
Najib Hossain <najibhossn@gmail.com>
2025-03-02 11:48:25 +0600

    MemberDashboard page height fix for larger screens

--- src/pages/MemberDashboard.jsx
+++ src/pages/MemberDashboard.jsx
@@ -155,7 +155,7 @@ const MemberDashboard = () => {
   }
 
   return (
-    <div className="h-[190vh] md:max-2xl:h-[130vh] w-full flex
+    <div className="h-[190vh] md:max-2xl:h-[140vh] w-full flex
      justify-center items-center">
       <div className="h-full w-[80%] flex flex-col justify-center
       items-center gap-20">
