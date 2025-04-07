dc78ae731bec0306e709c686bd39e83a172e059a
Najib Hossain <najibhossn@gmail.com>
2025-04-07 23:29:11 +0600

    Auth context file path update

--- src/utils/PrivateRoute.jsx
+++ src/utils/PrivateRoute.jsx
@@ -1,20 +1,21 @@
 import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from './AuthContext';
 
 /* eslint-disable react/prop-types */
 const PrivateRoute = ({ allowedRoles }) => {
+
   const { user, userRole } = useAuth();
 
   // If user is not logged in, redirect to login page
-  if (!user) return <Navigate to="/login" replace />;
+  if (!user) return <Navigate to="/login" replace />
 
   // If user role is not allowed, redirect to the homepage or error page
   if (allowedRoles && !allowedRoles.includes(userRole)) {
-    return <Navigate to="/" replace />;
+    return <Navigate to="/" replace />
   }
 
   // Render children if all checks pass
-  return <Outlet />;
+  return <Outlet />
 };
 
 export default PrivateRoute;
